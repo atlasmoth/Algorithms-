bd375c9cb7d50b004f9e7f24b4368b7509c5a73e
atlasmoth <ebuka422@gmail.com>
2020-06-22 17:56:37 +0100

    worked solutions to primalty and fibonacci

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-function sumArray(...arr) {
-  return arr.reduce((acc, curr) => {
-    return acc + curr;
-  });
-  // linear time cos the loop runs as many times as there are items in the array
+// function fib(num) {
+//   let arr = [];
+//   for (let i = 0; i < num; i++) {
+//     if (i === 0 || i === 1) {
+//       arr.push(i);
+//     } else {
+//       arr[i] = arr[i - 1] + arr[i - 2];
+//     }
+//   }
+//   return arr[num - 1];
+// }
+
+// console.log(fib(20));
+
+// it's linear time, nothing is multiplying itself bruh.
+
+function isPrime(num) {
+  let bool;
+  for (let i = 2; i < Math.round(num / 2); i++) {
+    if (bool === false) return bool;
+    bool = num % i > 0;
+  }
+  return bool;
 }
-console.log(sumArray(1, 3, 10, 50));
+console.log(isPrime(9));
