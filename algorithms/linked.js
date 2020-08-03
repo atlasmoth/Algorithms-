46c968bf080f73acdaaebcf8ff1cad4d23ff113b
atlasmoth <ebuka422@gmail.com>
2020-08-03 21:44:20 +0100

    working on linked lists

--- algorithms/linked.js
+++ algorithms/linked.js
@@ -27,7 +27,7 @@ class Linked {
     }
   }
   prepend(data = crypto.randomBytes(10).toString("hex")) {
-    const node = new Node(`${data}`);
+    const node = new Node(data);
     node.next = this.head;
     if (!this.head) {
       this.lastVal = node;
@@ -63,24 +63,81 @@ class Linked {
   }
   unshift() {
     if (!this.head || !this.head.next) {
-      this.pop();
+      return this.pop();
     }
     let holder = this.head;
     this.head = holder.next;
     return holder;
   }
   insert(data = crypto.randomBytes(10).toString("hex"), location) {
+    if (!this.head) {
+      return this.append(data);
+    }
     const node = new Node(data);
+    let count = 0;
+    let prevNode = this.head;
+    let holder = prevNode.next;
+
+    while (true) {
+      if (count === location && holder !== undefined) {
+        prevNode.setNext(node);
+        node.setNext(holder);
+        break;
+      } else if (holder === undefined) {
+        this.lastVal.setNext(node);
+        this.lastVal = node;
+        break;
+      }
+
+      prevNode = prevNode.next;
+      holder = prevNode.next;
+      count++;
+    }
+  }
+  search(index) {
+    if (!this.head) return;
     let count = 0;
     let current = this.head;
     while (true) {
-      let currentNode = current;
-      let nextNode = currentNode.next;
-      let finalNode = nextNode.next;
-      if (count === location) {
+      if (current === undefined) {
+        break;
       }
-      current = current.next;
+      if (count === index) {
+        return current;
+      }
       count++;
+      current = current.next;
+    }
+  }
+  specificRemove(index) {
+    if (!this.head) return;
+
+    let prevNode = this.head;
+    let nextNode = prevNode.next;
+
+    let count = 0;
+
+    while (true) {
+      if (nextNode === undefined && count !== index) {
+        return;
+      }
+      if (count === index) {
+        if (nextNode === undefined) {
+          prevNode.setNext(undefined);
+          this.lastVal = prevNode;
+          return nextNode;
+        } else {
+          let holder = nextNode;
+
+          prevNode.setNext(nextNode.next);
+          this.lastVal = prevNode.next;
+          return holder;
+        }
+      }
+      count++;
+
+      prevNode = prevNode.next;
+      nextNode = nextNode.next;
     }
   }
 }
